cb48410ae1ed45ecbb138491c6d515f12f8fa89d
irisjitomo <irisjitomo@eyahoo.com>
2019-10-29 14:12:50 -0400

    home tweaks

--- front-end/src/Components/home.js
+++ front-end/src/Components/home.js
@@ -1,16 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CarPic from '../Images/Car.jpg'
 import GhostTownPic1 from '../Images/ghostTownHome1.jpg'
 import './home.scss'
 
-const Home = () => {
+const Home = props => {
+        const credentials = {
+            username: '',
+            password: ''
+		}
+		
+		const [register, setRegister] = useState(credentials)
+
+		const handleChange = e => {
+			e.persist()
+			setRegister({
+				...register,
+				[e.target.name] : e.target.value
+			})
+		}
+
+		const submitRegister = e => {
+			e.preventDefault();
+			// there should be an axios post request here
+			setRegister(credentials)
+			// props.history.psuh('/home')
+		}
+
+
 	return (
 		<div className='Home'>
+			<div className="top-content">
 			<div className="titles">
 				<h1>Welcome To Ghost Town Locator</h1>
                 <h2>Midwest Edition</h2>
 				<h2>The place to locate and share ghost towns around the midwest</h2>
 			</div>
+
+            <div className='signup'>
+				<form className='form'>
+					<h1>Register?</h1>
+					<div className="form__group">
+					<input 
+					name="username"
+					placeholder="username"
+					value={register.username}
+					onChange={handleChange}
+					className="form__input"
+					/>
+					</div>
+
+					<div className="form__group">
+					<input
+					name="password"
+					placeholder="password"
+					value={register.password}
+					onChange={handleChange}
+					className="form__input"
+					/>
+					</div>
+					<div className="button-container">
+						<button className='btn' type='button'>Register</button>
+						<button className='btn' type='button'>Login</button>
+						{/* login needs to have an onClick to route to /login */}
+					</div>
+				</form>
+            </div>
+			</div>
+
 			<div className="paragraph1">
                 <div className='paragraph-top'>
 				<p>
