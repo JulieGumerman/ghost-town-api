d064026c49c832cd7e31ef5d9609fc7ac60c3dec
irisjitomo <irisjitomo@eyahoo.com>
2019-10-31 23:13:14 -0400

    demonstration of pull request

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,6 +7,7 @@ import Header from './Components/header/header';
 import Footer from './Components/footer/footer';
 import Home from './Components/home/home';
 import Login from './Components/login/login';
+import Map from './Components/map-overview/map-overview'
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
       <Header />
       <Route exact path='/' component={Home} />
       <Route exact path='/login' component={Login}/>
+      <Route exact path='/map-overview' component={Map} />
       <Footer />
     </div>
   );
---
+++ front-end/src/Components/map-overview/map-overview.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+
+
+const MapOverview = props => {
+    return (
+        <div className='map'>
+            <h1>Map Overview</h1>
+        </div>
+    )
+}
+
+export default MapOverview
