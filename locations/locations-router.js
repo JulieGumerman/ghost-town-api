5c0e46414cd8a0ce21de5fbf2359cc6beef3659a
bodhibrdr <julie.gumerman@gmail.com>
2019-11-10 21:43:56 -0700

    validation

--- locations/locations-router.js
+++ locations/locations-router.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const locRoute = express.Router();
-const Locations = require("./locations-model")
+const Locations = require("./locations-model");
+const validate = require("../auth-and-middleware/validate");
 
 
 
-locRoute.get("/", (req, res) => {
+locRoute.get("/", validate, (req, res) => {
     Locations.get()
         .then(locations => res.status(200).json(locations))
         .catch(err => res.status(500).json(err))
 })
 
-locRoute.get("/:id", (req, res) => {
+locRoute.get("/:id", validate, (req, res) => {
     Locations.getById(req.params.id)
         .then(location => {
             res.status(200).json(location)
@@ -20,7 +21,7 @@ locRoute.get("/:id", (req, res) => {
         })
 })
 
-locRoute.post("/", (req, res) => {
+locRoute.post("/", validate, (req, res) => {
     Locations.addLocation(req.body)
         .then(location => res.status(200).json(location))
         .catch(err => res.status(500).json({ message: "town cannot be added at this time"}))
